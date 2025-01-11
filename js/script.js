c162835961f3092e821afe36407134a370373b7b
Greenhoh1 <derekjlucier@gmail.com>
2025-01-11 12:22:17 -0500

    rermoved roblox

--- js/script.js
+++ js/script.js
@@ -203,12 +203,6 @@ const games = [
         description: 'a game where you have to collect coins to win.',
         thumbnail: 'games/SuperScratchBros.Scrultimate/Thumbnail.png',
         link: 'games/SuperScratchBros.Scrultimate/index.html'
-    },
-    {
-        title: 'Roblox',
-        description: 'a game where you can play games made by other people.',
-        thumbnail: 'games/Roblox/Thumbnail.png',
-        link: 'games/Roblox/static/index.html'
     }
 ];
 
